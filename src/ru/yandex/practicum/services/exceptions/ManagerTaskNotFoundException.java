7a22e7685ebd993dc8eae053513053bdc84d891c
kirshumir01 <kirshumir@yandex.ru>
2024-04-02 17:22:53 +0300

    fix (автотесты git): исправить ошибки по codestyle

--- src/ru/yandex/practicum/services/exceptions/ManagerTaskNotFoundException.java
+++ src/ru/yandex/practicum/services/exceptions/ManagerTaskNotFoundException.java
@@ -6,6 +6,7 @@ public class ManagerTaskNotFoundException extends RuntimeException {
     public ManagerTaskNotFoundException(String message) {
         super(message);
     }
+
     public ManagerTaskNotFoundException(final TaskType type) {
         this("Задача типа " + type.toString() + " не найдена в менеджере");
     }
